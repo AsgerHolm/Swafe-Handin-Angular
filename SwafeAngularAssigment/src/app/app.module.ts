6161cccd8f65126fc484688fdb10756e9d5ceb11
AsgerHolm <47742247+AsgerHolm@users.noreply.github.com>
2021-10-11 13:03:01 +0200

    navigationbar works

--- SwafeAngularAssigment/src/app/app.module.ts
+++ SwafeAngularAssigment/src/app/app.module.ts
@@ -3,14 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationBarComponent
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
